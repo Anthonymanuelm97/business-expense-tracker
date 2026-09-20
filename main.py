04e879233a7e21da4589f94fb770cad92cb3035d
anthonymanuelm97 <anthonymanuelm97@gmail.com>
2026-09-20 03:21:01 -0400

    Add initial expense data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from business-expense-tracker!")
+business_expenses = [
+    {"description": "Store rent", "amount": 15000.00, "category": "Rent"},
+    {"description": "Supply purchase", "amount": 3500.00, "category": "Supplies"},
+    {"description": "Social media advertising",
+        "amount": 1200.00, "category": "Marketing"},
+]
 
-
-if __name__ == "__main__":
-    main()
