15b7fab2c2c09f449685907965242f84223c02c1
Antonio Melo <Anthonymanuelm97@gmail.com>
2026-09-21 14:15:25 -0400

    Add add_expense and view_expenses functions

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ business_expenses = [
         "amount": 1200.00, "category": "Marketing"},
 ]
 
+def add_expense(expenses, description, amount, category):
+    new_expense = {"description": description, "amount": amount, "category": category}
+    expenses.append(new_expense)
+    return new_expense
+
+def view_expenses(expenses):
+    for expense in expenses:
+        print(f"Description: {expense['description']}, Amount: ${expense['amount']:.2f}, Category: {expense['category']}")
+
+
+view_expenses(business_expenses)
+
+add_expense(business_expenses, "Website hosting", 200.00, "IT Services")
+
+view_expenses(business_expenses)
